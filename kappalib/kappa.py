8dfa63ffca7c00da9a0c5b627e2f6ca2e02aedcd
Cristiano Moraes Bilacchi Azarias <cristiano@MacBook-Air-de-Cristiano-3.local>
2020-01-06 01:52:26 -0300

    setup

--- kappalib/kappa.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-def scatter(x,y,jitter=.75,errBar='std',facecolor='none',edgecolor='firebrick',alpha=1,s=50):
-  np.random.seed(23)
-  X = x + np.random.randn(*np.shape(x))*jitter/10
-  plt.scatter(X,y,facecolor=facecolor,edgecolor=edgecolor,alpha=alpha,s=s)
